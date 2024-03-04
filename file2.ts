d1289c2417594a98a00f88a4955b98d0ef8a1a3e
Syeda Muntaha Shah <161372867+Syedamuntahashah@users.noreply.github.com>
2024-03-04 18:43:27 +0500

    Add files via upload
    Exercise 2 Answers 
    Assignment

---
+++ file2.ts
@@ -0,0 +1,18 @@
+// Question 6:
+
+console.log(3 + 5);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+
+// Question 7:
+
+console.log(10 - 2);
+console.log(3 + 5);
+console.log(2 * 4);
+console.log(16 / 2);
+
+// Question 8;
+
+let favoriteNumber: number = 6;
+console.log(`My favorite number is. ${favoriteNumber}.` );
